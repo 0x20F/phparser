e5eccc65e9dcc1817057e4120cacd9b3ee264c6d
Poly <alexhexan22@gmail.com>
2019-12-02 19:10:12 +0100

    Test out returning a full struct

--- src/pulse.rs
+++ src/pulse.rs
@@ -1,5 +1,7 @@
 mod parser;
 
 fn main() {
-    parser::run("This should be an array of directories");
+    let block = parser::run("This should be an array of directories");
+
+    println!("Parser finished file is: {}", block.filename_parent())
 }
