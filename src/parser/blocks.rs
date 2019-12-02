ee3423bc11fd5a45386f754ba6c0e0ba803f833e
Poly <alexhexan22@gmail.com>
2019-12-02 19:59:37 +0100

    Move function struct to its own file

---
+++ src/parser/blocks.rs
@@ -0,0 +1,46 @@
+/// A parsed function blocks data
+/// TODO: Some of these can be private so you can use setters with custom functionality
+pub struct FunctionBlock {
+    pub name: String,
+    pub visibility: String,
+    pub params: Vec<String>,
+    pub hash: String,
+    pub children: Vec<String>,
+    pub parent: String
+}
+
+impl FunctionBlock {
+    
+    /// Parse the given function data to compile a list of important
+    /// info about a function
+    /// 
+    /// TODO: Make it happen
+    pub fn new(data: &str) -> FunctionBlock {
+        FunctionBlock {
+            name: String::from(data),
+            visibility: String::from(data),
+            params: vec![],
+            hash: String::from("12t809j39t1j3t12qwe12e12t23timorgkm"),
+            children: vec![],
+            parent: String::from("This/Very/Long/Path/To/A/file.php")
+        }
+    }
+    
+    
+    
+    /// Gets the name of the file this function is in
+    /// 
+    /// # Example
+    /// ```
+    /// let block = FunctionBlock {
+    ///     // Other params...
+    ///     parent: "Path/To/File/Containing/This/file.php"
+    /// };
+    /// 
+    /// block::filename_parent() // returns "file.php"
+    /// ```
+    pub fn filename_parent(&self) -> String {
+        let vec: Vec<&str> = self.parent.split("/").collect();
+        vec.last().unwrap().to_string()
+    }
+}
