a2fc9d689415043a5b7a669c96c9ba870a5e257b
Poly <alexhexan22@gmail.com>
2019-12-02 19:59:54 +0100

    Properly instantiate function block with some data

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,48 +1,12 @@
-/// A parsed function blocks data
-pub struct FunctionBlock {
-    name: String,
-    visibility: String,
-    params: Vec<String>,
-    hash: String,
-    children: Vec<String>,
-    parent: String
-}
+mod blocks;
 
-impl FunctionBlock {
-    /// Gets the name of the file this function is in
-    /// 
-    /// # Example
-    /// ```
-    /// let block = FunctionBlock {
-    ///     name: String::from("getPancakes"),
-    ///     visibility: String::from("public"),
-    ///     params: ["a", "b", "c"],
-    ///     hash: md5::compute(b"this is all the function data into one string or something"),
-    ///     children: ["getFlour", "getMilk", "getSugar"],
-    ///     parent: "Path/To/File/Containing/This/file.php"
-    /// };
-    /// 
-    /// block::filename_parent() // returns "file.php"
-    /// ```
-    pub fn filename_parent(&self) -> String {
-        let vec: Vec<&str> = self.parent.split("/").collect();
-        vec.last().unwrap().to_string()
-    }
-}
-
+use blocks::FunctionBlock;
 
 
 pub fn run(dir: &str) -> FunctionBlock {
     println!("Indexing the following paths ({})", dir);
 
-    FunctionBlock {
-        name: String::from(dir),
-        visibility: String::from(dir),
-        params: vec![],
-        hash: String::from("12t809j39t1j3t12qwe12e12t23timorgkm"),
-        children: vec![],
-        parent: String::from("This/Very/Long/Path/To/A/file.php")
-    }
+    FunctionBlock::new(dir)
 }
 
 
