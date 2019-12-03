3aa1e1b1946859cf3f675648ba93f4394ca0b166
Poly <alexhexan22@gmail.com>
2019-12-03 12:36:52 +0100

    Find all files in a given directory

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,19 +1,43 @@
 mod function;
 mod file;
 
-use function::Function;
+use function::FunctionModel as Function;
+use walkdir::{WalkDir, DirEntry};
 
 
 
-pub fn run(dirs: Vec<&str>) -> Function {
+pub fn run(dirs: Vec<&str>) {
     println!("Indexing the following paths ({:?})", dirs);
 
-    Function::new(dirs[0])
+    files("/Users/alex.hexan/repo/journal_sys/tests/unit");
 }
 
 
+
+fn is_php_file(entry: &DirEntry) -> bool {
+    entry
+        .file_name()
+        .to_str()
+        .map(|s| s.ends_with(".php"))
+        .unwrap_or(true)
+}
+
+
+
 /// Get all the files in a given directory, recursively
-pub fn files(dir: &str) {}
+pub fn files(dir: &str) {
+
+    let walker = WalkDir::new(dir);
+
+    for entry in walker {
+        let entry = entry.unwrap();
+
+        if is_php_file(&entry) {
+            println!("{}", entry.path().display());
+        }
+    }
+}
+
 
 
 /// Get all functions in a file
