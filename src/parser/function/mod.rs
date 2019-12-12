dce27ed66e7d2a793d9f9195df5a447483f1e31d
Poly <alexhexan22@gmail.com>
2019-12-12 08:56:42 +0100

    Functions can now parse all used functions inside of them

--- src/parser/function/mod.rs
+++ src/parser/function/mod.rs
@@ -4,15 +4,24 @@ use md5::Digest;
 use super::file::FileModel;
 
 
+
+lazy_static! {
+    static ref RE: Regex = Regex::new(r"(?m)(->|::)([A-Za-z_]+?)(\((?:.*?)\))").unwrap();
+}
+
+
+
 /// A parsed function blocks data
 /// TODO: Some of these can be private so you can use setters with custom functionality
 /// TODO: These shouldn't all be strings
 pub struct FunctionModel {
-    pub name: String,
-    pub visibility: String,
-    pub hash: Digest,
-    pub parent: String,
-    pub functions: Vec<String>,
+    pub name        : String,
+    pub visibility  : String,
+    pub hash        : Digest,
+    pub parent      : String,
+
+    functions       : Vec<String>,
+    function_refs   : Option<Vec<&'static FunctionModel>>
 }
 
 
@@ -21,9 +30,6 @@ impl FunctionModel {
     /// Parse the given function data to compile a list of important
     /// info about a function
     pub fn new(data: Vec<String>, parent: &FileModel) -> FunctionModel {
-        lazy_static! {
-            static ref RE: Regex = Regex::new(r"(->|::)[A-Za-z_]+?(\((?:.*?)\))").unwrap();
-        }
 
         // Declaration is always first line
         let declaration = data.first().unwrap().to_owned();
@@ -40,12 +46,24 @@ impl FunctionModel {
         let hash = md5::compute(&full_data);
 
 
+        let mut functions: Vec<String> = vec![];
+
+        for cap in RE.captures_iter(&full_data) {
+            let fn_name = cap.get(2).map_or("", |m| m.as_str());
+            functions.push(fn_name.to_string());
+        }
+
+        functions.sort_unstable();
+        functions.dedup();
+
+
         FunctionModel {
             name,
             visibility,
             hash,
             parent: parent.name().to_string(),
-            functions: vec![]
+            functions,
+            function_refs: None
         }
     }
 }
