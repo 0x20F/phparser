e7209da05ace1ba1d454fc645d9b407288033e9e
SirTheViking <alexhexan22@gmail.com>
2020-02-10 07:46:29 +0100

    Add tests to check if the file amount found is as expected

--- tests/parser_integration.rs
+++ tests/parser_integration.rs
@@ -5,3 +5,15 @@ use phparser::Parser;
 fn test_instance() {
     Parser::new();
 }
+
+
+#[test]
+fn test_parse_dir() {
+    let parser = Parser::new();
+
+    // Assert that the amount of files is as expected
+    // when things get parsed eventually
+    let files = parser.parse_dir("./tests/data/parse_dir_test");
+
+    assert_eq!(3, files.len());
+}
