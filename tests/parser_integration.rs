edb647954cbf688c69994fd40c94208cc9a38935
SirTheViking <alexhexan22@gmail.com>
2020-02-10 08:47:50 +0100

    Remove comment

--- tests/parser_integration.rs
+++ tests/parser_integration.rs
@@ -11,8 +11,6 @@ fn test_instance() {
 fn test_parse_dir() {
     let parser = Parser::new();
 
-    // Assert that the amount of files is as expected
-    // when things get parsed eventually
     let files = parser.parse_dir("./tests/data/parse_dir_test");
 
     assert_eq!(3, files.len());
