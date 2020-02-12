b127f3aec4601a2d3e69905ac47f0165c9d2083f
SirTheViking <alexhexan22@gmail.com>
2020-02-12 07:41:53 +0100

    Simple test to make sure the new definition can instantiate properly (test will change a lot)

--- tests/parser_integration.rs
+++ tests/parser_integration.rs
@@ -15,3 +15,26 @@ fn test_parse_dir() {
 
     assert_eq!(3, files.len());
 }
+
+
+#[test]
+fn test_parse_file() {
+    let parser = Parser::new();
+
+    // You'll have to assert the count of:
+    // classes
+    //      functions
+    // functions outside of classes
+    // namespace
+    // no-namespace
+    // dependencies
+    // dependents
+
+    // For now just check if you get a thing back
+    let file_path = String::from("./tests/data/parse_file_test/one.php");
+    let file = parser.parse_file(file_path);
+
+    let path = String::from("./tests/data/parse_file_test/one.php");
+
+    assert_eq!(path, file.path);
+}
