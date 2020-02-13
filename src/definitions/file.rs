0526a513b80df9fb62550d05c5ab6482b4724148
SirTheViking <alexhexan22@gmail.com>
2020-02-13 08:25:03 +0100

    Parse the file name on init

--- src/definitions/file.rs
+++ src/definitions/file.rs
@@ -1,14 +1,29 @@
 pub struct FileDef {
-    pub path: String
+    pub path: String,
+    pub name: String
 }
-
 
 
 impl FileDef {
     pub fn new(path: String) -> FileDef {
 
+        let name = FileDef::parse_name(&path);
+
         FileDef {
-            path
+            path,
+            name
         }
+    }
+
+
+
+
+
+    fn parse_name(path: &String) -> String {
+        let pieces: Vec<&str> = path
+            .split(&['/', '\\'][..])
+            .collect();
+
+        pieces.last().unwrap().to_string()
     }
 }
