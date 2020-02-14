fe5f11ea07677b3edc84255bea944d7646264764
SirTheViking <alexhexan22@gmail.com>
2020-02-14 08:50:11 +0100

    Open file once, all parsing will be done on that one stream, just rewind it

--- src/definitions/file.rs
+++ src/definitions/file.rs
@@ -1,13 +1,22 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::{BufReader, SeekFrom};
+use std::path::{Path, PathBuf};
+use std::fmt::Display;
+
 pub struct FileDef {
-    pub path: String,
-    pub name: String
+    pub path: PathBuf,
+    pub name: String,
+
+    // Save opened file here and close it when done?
 }
 
 
 impl FileDef {
-    pub fn new(path: String) -> FileDef {
+    pub fn new(path: PathBuf) -> FileDef {
 
         let name = FileDef::parse_name(&path);
+        let mut stream = FileDef::open_file(&path);
 
         FileDef {
             path,
@@ -16,14 +25,29 @@ impl FileDef {
     }
 
 
-
-
-
-    fn parse_name(path: &String) -> String {
+    fn parse_name(path: &PathBuf) -> String {
         let pieces: Vec<&str> = path
+            .to_str()
+            .unwrap()
             .split(&['/', '\\'][..])
             .collect();
 
         pieces.last().unwrap().to_string()
+    }
+
+
+    fn open_file(path: &PathBuf) -> BufReader<File> {
+        // Make sure you fail gracefully here!
+        let f = File::open(path).unwrap();
+
+        let mut f = BufReader::new(f);
+
+        f
+    }
+
+
+    fn rewind(buffer: &mut BufReader<File>) {
+        // Seek back to the start so other things can use this same buffer
+        buffer.seek(SeekFrom::Start(0)).unwrap();
     }
 }
