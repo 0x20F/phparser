75b473b9585bef427cb20900baf0e3d2119d14e9
SirTheViking <alexhexan22@gmail.com>
2020-02-15 14:52:38 +0100

    Add integration tests for lexer

---
+++ tests/data/lexer_tests/namespace_test/namespace.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace test\space;
+
+
+class one
+{
+
+}
---
+++ tests/lexer_integration.rs
@@ -0,0 +1,26 @@
+use phparser::lexer::{Lexer, Tokens};
+use phparser::definitions::FileStream;
+use std::path::{PathBuf, Path};
+
+
+// Open a simple file stream for all functions to use
+fn setup_stream(path: &str) -> FileStream {
+    let p = Path::new(path);
+    FileStream::new(&p.to_path_buf())
+}
+
+
+
+
+
+#[test]
+fn test_find_namespace() {
+    let mut file = setup_stream("./tests/data/lexer_tests/namespace_test/namespace.php");
+
+    let namespace = Lexer::find_namespace(&mut file);
+
+    match namespace {
+        Tokens::NotFound() => panic!("Should've found a namespace in namespace.php"),
+        _ => ()
+    };
+}
