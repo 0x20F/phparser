0ef3d94274a19895338ddc7846ddd65c1740f420
SirTheViking <alexhexan22@gmail.com>
2020-02-16 11:59:55 +0100

    Try and lex a different way

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -4,9 +4,19 @@ use std::io::{Read, BufRead};
 
 
 pub struct Token {
-    token: TokenType,
-    line: u64,
-    symbol: String
+    pub token: TokenType,
+    pub line: u64,
+    pub symbol: String
+}
+
+impl Token {
+    pub fn new(token: TokenType, line: u64, symbol: String) -> Token {
+        Token {
+            token,
+            line,
+            symbol
+        }
+    }
 }
 
 
@@ -28,8 +38,18 @@ pub struct Lexer {}
 impl Lexer {
     pub fn tokenize(stream: &mut FileStream) -> Vec<Token> {
         let mut tokens: Vec<Token> = vec![];
+        let mut stack: Vec<u32> = vec![];
 
         let mut line_number = stream.current_line();
+
+        /*
+            Flags:
+
+            n => namespace already done
+        */
+
+        let (mut n) = (false);
+
         
         for line in stream.buffer.by_ref().lines() {
             let line = match line {
@@ -37,12 +57,37 @@ impl Lexer {
                 _ => break
             };
 
-            
+            // Parse namespace if it hasn't been parsed already
+            if !n && line.starts_with("namespace") {
+                let token = Lexer::parse_namespace(line, line_number);
+                tokens.push(token);
+                n = true;
+            }
+
+
 
             line_number = line_number + 1;
         }
 
 
         tokens
+    }
+
+
+    pub fn parse_namespace(line_text: String, line_number: u64) -> Token {
+        let namespace = line_text
+            .trim()
+            .split_whitespace()
+            .last()
+            .unwrap()
+            .trim_end_matches(';');
+
+        let token = Token::new(
+            TokenType::Namespace,
+            line_number,
+            namespace.to_string()
+        );
+
+        token
     }
 }
--- tests/lexer_integration.rs
+++ tests/lexer_integration.rs
@@ -8,3 +8,15 @@ fn setup_stream(path: &str) -> FileStream {
     let p = Path::new(path);
     FileStream::new(&p.to_path_buf())
 }
+
+
+#[test]
+fn test_parse_namespace() {
+    let namespace = "namespace This\\Is\\a\\PHP\\◊°∆˝¬º\\namespace;";
+    let line = 50;
+
+    let parsed = Lexer::parse_namespace(namespace.to_string(), line);
+
+    assert_eq!(parsed.symbol, "This\\Is\\a\\PHP\\◊°∆˝¬º\\namespace");
+    assert_eq!(parsed.line, line);
+}
