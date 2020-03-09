26ef5f8e5cf54403a305bd18658081d2f627647a
Poly <alexhexan22@gmail.com>
2020-03-09 07:35:53 +0100

    Remove parameters token for now

--- src/lexer/token.rs
+++ src/lexer/token.rs
@@ -9,11 +9,9 @@ pub enum Token {
     MethodStart(u64),
     MethodName(u64, String),
     MethodPrivacy(u64, String),
-    MethodParameters(u64, String),
     MethodEnd(u64),
 
     FunctionStart(u64),
     FunctionName(u64, String),
-    FunctionParameters(u64, String),
     FunctionEnd(u64)
 }
