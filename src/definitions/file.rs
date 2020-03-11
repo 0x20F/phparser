119b66993b7e3944a75880254bc4c8f14b578d51
Poly <alexhexan22@gmail.com>
2020-03-11 07:04:28 +0100

    Separate function object creation into separate function

--- src/definitions/file.rs
+++ src/definitions/file.rs
@@ -117,16 +117,44 @@ impl FileDef {
     }
 
 
-    fn build_class<I>(tokens: &mut I)
+    fn build_class<I>(mut tokens: &mut I)
         where I: Iterator<Item = Token>
     {
         let mut token = tokens.next().unwrap();
 
         // Loop through tokens until ClassEnd is reached
+        // This should all build a ClassDef object
         loop {
             match token {
                 Token::ClassName(_, n) => println!("Theres a class name: {}", n),
+
+                Token::FunctionStart(_) => Self::build_function(&mut tokens),
+
                 Token::ClassEnd(_) => println!("Class end directly"),
+                _ => println!("There are other things")
+            }
+
+            if let Some(t) = tokens.next() {
+                token = t;
+            } else {
+                break;
+            }
+        }
+    }
+
+
+    fn build_function<I>(tokens: &mut I)
+        where I: Iterator<Item = Token>
+    {
+        let mut token = tokens.next().unwrap();
+
+        // Loop through tokens until FunctionEnd is reached
+        // This should all build a FunctionDef object
+        loop {
+            match token {
+                Token::FunctionName(_, n) => println!("Found function with name: {}", n),
+                Token::FunctionPrivacy(_, p) => println!("And privacy: {}", p.unwrap()),
+                Token::FunctionEnd(_) => println!("Function ends now"),
                 _ => println!("There are other things")
             }
 
