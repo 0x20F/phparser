9699251005181f6b86fb7a7bdf68303dc72a4e29
Poly <alexhexan22@gmail.com>
2020-03-12 09:03:15 +0100

    Add setter for name

--- src/definitions/class.rs
+++ src/definitions/class.rs
@@ -1,3 +1,9 @@
-pub struct ClassDef {}
+pub struct ClassDef {
+    name: String,
+}
 
-impl ClassDef {}
+impl ClassDef {
+    pub fn set_name(&mut self, name: String) {
+        self.name = name;
+    }
+}
